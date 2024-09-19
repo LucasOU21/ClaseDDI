3f6d3f080162dd8085baa283331c83ced7e79d6e
LOUBIT <103092618+LucasOU21@users.noreply.github.com>
2024-09-19 10:32:26 +0200

    First Commit for my Second Project

---
+++ MySecondProject/src/main/java/com/mycompany/mysecondproject/MySecondProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mysecondproject;
+
+/**
+ *
+ * @author LOU01
+ */
+public class MySecondProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
