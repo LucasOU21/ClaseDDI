8ea7fba926a43dc55f0fc5028783193c2b72105c
LOUBIT <103092618+LucasOU21@users.noreply.github.com>
2024-09-19 09:34:07 +0200

    First Commit for my App Project

---
+++ MyFirstProject/src/main/java/com/mycompany/myfirstproject/MyFirstProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myfirstproject;
+
+/**
+ *
+ * @author LOU01
+ */
+public class MyFirstProject {
+
+    public static void main(String[] args) {
+        
+    }
+}
